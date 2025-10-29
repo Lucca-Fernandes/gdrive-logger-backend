5506dab6793693c60b00dc1d4e60ec1242248813
Lucca-Fernandes <lfdc212@gmail.com>
2025-10-29 17:43:51 -0300

    Use Secret Files do Render

--- src/drive.js
+++ src/drive.js
@@ -1,26 +1,24 @@
 // src/drive.js
 const { google } = require('googleapis');
-const path = require('path');
 
 let driveClient = null;
 
 async function authenticate() {
   try {
-    const keyFile = path.join(process.cwd(), 'service-account.json');
-    
-    if (!require('fs').existsSync(keyFile)) {
-      throw new Error(`ARQUIVO NÃO ENCONTRADO: ${keyFile}`);
-    }
+    console.log('Iniciando autenticação via Secret Files...');
+
+    // O Render monta Secret Files em /etc/secrets/
+    const keyFile = '/etc/secrets/service-account.json';
 
     const auth = new google.auth.GoogleAuth({
-      keyFile,
+      keyFilename: keyFile,
       scopes: ['https://www.googleapis.com/auth/drive.readonly']
     });
 
     const client = await auth.getClient();
     driveClient = google.drive({ version: 'v3', auth: client });
-    
-    console.log('Google Drive autenticado com sucesso (via keyFile)!');
+
+    console.log('Google Drive autenticado com sucesso (Secret Files)!');
   } catch (err) {
     console.error('Erro na autenticação:', err.message);
     throw err;
